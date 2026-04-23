8f43f88ff87f12944e954659d4c1385e56c35990
Matheus Sombra <msombra10@gmail.com>
2026-04-23 18:57:35 -0300

    criar componente Logo

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,15 @@
+import { TimerIcon } from 'lucide-react';
+import styles from './styles.module.css';
+
+export const Logo = () => {
+
+    return (
+        <div className={styles.logo}>
+            <a className={styles.logoLink} href="#">
+                <TimerIcon />
+                <span>Chronos</span>
+            </a>
+        </div>
+    );
+
+};
