c8f343727b835d686210e0c04af6f9120bfc86c5
Matheus Sombra <msombra10@gmail.com>
2026-04-25 13:38:07 -0300

    implementar alternânica entre temas dark e light na página

--- src/components/Menu/button.tsx
+++ src/components/Menu/button.tsx
@@ -1,13 +1,13 @@
 import styles from './styles.module.css';
 
 type ContainerProps = {
-    children: React.ReactNode;
-};
+    children: React.ReactNode
+} & React.ComponentProps<'button'>;
 
-export const Button = ({ children }: ContainerProps) => {
+export const Button = ({ children, ...props }: ContainerProps) => {
 
     return (
-        <button type='button' className={styles.menuLink}>
+        <button type='button' className={styles.menuLink} {...props}>
             { children }
         </button>
     );
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,29 +1,72 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
 import { Button } from './button';
+import { useState, useEffect } from 'react';
+
+type AvailableThemes = 'dark' | 'light';
 
 export const Menu = () => {
+
+    const [theme, setTheme] = useState<AvailableThemes>(() => {
+        return localStorage.getItem('theme') as AvailableThemes || 'dark';
+    });
+
+    const handleThemeChange = () => {
+        setTheme(prevTheme => {
+            return prevTheme === 'dark' ? 'light' : 'dark';
+        });
+    };
+
+    // #### --- Explicação sobre useEffect --- ####
+
+    // useEffect(() => {
+    //     console.log('useEffect sem dependências', Date.now());
+    // }); // Executado toda vez que o componente renderiza na tela
+
+    // useEffect(() => {
+    //     console.log('useEffect com array de deps vazio', Date.now());
+    // }, []); // Executa apenas quando o React monta o componente na tela pela primeira vez
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+    }, [theme]); // Executa apenas quando o valor de theme muda
+
+    // ############################################
 
     return (
         <nav className={styles.menu}>
             {/* Home */}
-            <Button>
+            <Button
+                aria-label="Ir para a Home"
+                title="Ir para a Home"
+            >
                 <HouseIcon />
             </Button>
 
             {/* Histórico */}
-            <Button>
+            <Button
+                aria-label="Ver Histórico"
+                title="Ver Histórico"
+            >
                 <HistoryIcon />
             </Button>
 
             {/* Configurações */}
-            <Button>
+            <Button
+                aria-label="Ir para página de Configurações"
+                title="Ir para página de Configurações"
+            >
                 <SettingsIcon />
             </Button>
 
             {/* Tema */}
-            <Button>
-                <SunIcon />
+            <Button
+                aria-label="Mudar Tema"
+                title="Mudar Tema"
+                onClick={handleThemeChange}
+            >
+                {theme === 'dark' ? <SunIcon /> : <MoonIcon />}
             </Button>
         </nav>
     );
