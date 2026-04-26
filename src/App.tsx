e0aa7a84ae5adbced0e31abe0a73f83311d5c57f
Matheus Sombra <msombra10@gmail.com>
2026-04-26 12:19:55 -0300

    deletar conteúdo anterior e inserir somente o componente Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
-import { Container } from "./components/Container";
-import { CountDown } from "./components/CountDown";
-import { Logo } from "./components/Logo";
-import { Menu } from "./components/Menu";
+import { Home } from "./pages/Home";
 import './styles/global.css';
 import './styles/theme.css';
-import { Footer } from "./components/Footer";
 
 export const App = () => {
-
-    return (
-        <>
-            <Container>
-                <Logo />
-            </Container>
-
-            <Container>
-                <Menu />
-            </Container>
-
-            <Container>
-                <CountDown />
-            </Container>
-
-            <Container>
-                <Footer />
-            </Container>
-        </>
-    );
+    return <Home />;
 };
