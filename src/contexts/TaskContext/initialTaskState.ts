b9e79aadc1c7b9d9ae3eb3d50a4f08605983b99a
Matheus Sombra <msombra10@gmail.com>
2026-04-27 19:02:26 -0300

    alterar chave task para tasks

--- src/contexts/TaskContext/initialTaskState.ts
+++ src/contexts/TaskContext/initialTaskState.ts
@@ -1,7 +1,7 @@
 import type { TaskStateModel } from "../../models/TaskStateModel";
 
 export const initialTaskState: TaskStateModel = {
-    task: [],
+    tasks: [],
     secondsRemaining: 0,
     formattedSecondsRemaining: '00:00',
     activeTask: null,
