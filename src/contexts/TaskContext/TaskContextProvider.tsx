734accd77642eb66c849eff6191b919225d393cc
Matheus Sombra <msombra10@gmail.com>
2026-04-27 12:14:03 -0300

    criar pasta contexts e arquivos de contextos da aplicação

---
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { TaskContext } from "./TaskContext";
+import { initialTaskState } from "./initialTaskState";
+
+type TaskContextProviderProps = {
+    children: React.ReactNode;
+};
+
+export const TaskContextProvider = ({ children }: TaskContextProviderProps) => {
+
+    const [state, setState] = useState(initialTaskState);
+    
+    return (
+        <TaskContext.Provider value={{ state, setState }}>
+            {children}
+        </TaskContext.Provider>
+    );
+}
