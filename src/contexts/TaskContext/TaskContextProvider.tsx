9be670566a427f0a04f0a71e23fbf91909260458
Matheus Sombra <msombra10@gmail.com>
2026-05-07 11:18:34 -0300

    implementar initialState no local storage do navegador

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -5,6 +5,7 @@ import { taskReducer } from "./taskReducer";
 import { TimerWorkerManager } from "../../workers/TimerWorkerManager";
 import { TaskActionTypes } from "./taskActions";
 import { loadBeep } from "../../utils/loadBeep";
+import type { TaskStateModel } from "../../models/TaskStateModel";
 
 type TaskContextProviderProps = {
     children: React.ReactNode;
@@ -12,7 +13,21 @@ type TaskContextProviderProps = {
 
 export const TaskContextProvider = ({ children }: TaskContextProviderProps) => {
 
-    const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+    const [state, dispatch] = useReducer(taskReducer, initialTaskState, () => {
+        const storageState = localStorage.getItem('state')
+        ;
+
+        if (storageState === null) return initialTaskState;
+
+        const parsedStorageState = JSON.parse(storageState) as TaskStateModel;
+
+        return {
+            ...parsedStorageState,
+            activeTask: null,
+            secondsRemaining: 0,
+            formattedSecondsRemaining: '00:00'
+        };
+    });
     const playBeepRef = useRef<() => void | null>(null);
 
     const worker = TimerWorkerManager.getInstance();
@@ -40,6 +55,8 @@ export const TaskContextProvider = ({ children }: TaskContextProviderProps) => {
     });
     
     useEffect(() => {
+        localStorage.setItem('state', JSON.stringify(state));
+
         if (!state.activeTask) worker.terminate();
 
         document.title = `${state.formattedSecondsRemaining} - Chronos Pomodoro`;
